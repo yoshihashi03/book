2a206f653c766acdc8af42d1670270563998c92b
vagrant <vagrant@localhost.localdomain>
2020-11-18 01:40:17 +0000

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  
 end
